7290aa212304a335d3f14cc6ebf5dcd6a53282c7
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-08-22 18:01:35 -0400

    more work

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,24 +1,21 @@
 class ListingsController < ApplicationController
 
-  before_action :set_city, :only [:show, :edit, :update, :destroy]
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
 
   def index
-    @cities=City.all
+    @listings=Listing.all
   end
 
   def show
-    @city=City.find(params[:id])
-    @neighborhoods=@city.neighborhoods
   end
 
   def new
-    @city=City.new
-    @neighborhoods=Neighborhood.all
+    @listing=Listing.new
   end
 
   def create
-    @city=City.create(city_params)
-    redirect_to city_path(@city)
+    @listing=Listing.create(listing_params)
+    redirect_to listing_path(@listing)
   end
 
   def edit
@@ -33,7 +30,11 @@ class ListingsController < ApplicationController
   private
 
   def listing_params
-    params.require(:city).permit(:title, :description, :address, :listing_type, :price)
+    params.require(:listing).permit(:title, :description, :address, :listing_type, :price)
+  end
+
+  def set_listing
+    @listing=Listing.find(params[:id])
   end
 
 end
